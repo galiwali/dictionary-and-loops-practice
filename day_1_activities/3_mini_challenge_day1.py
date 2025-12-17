638cf5b5139f60bdc4ef84e2627576616358936c
galiwali <maringaliany@gmail.com>
2025-12-17 09:32:55 -0600

    Update 3_mini_challenge_day1.py

--- day_1_activities/3_mini_challenge_day1.py
+++ day_1_activities/3_mini_challenge_day1.py
@@ -1,18 +1,51 @@
 # Project Prompt:
 
 import student_data
+CPSID= input("Enter CPS ID")
+FName = input("Enter First name")
+LName = input("Enter last name")
+MName = input("Enter middle name")
+HR =input("Enter homeroom")
+GL = input("Enter Grade Level")
+primary_email = input("Enter Primary Email")
+secondary_email = input("Enter Secondary Email")
+print("Program running. Press 'q' to exit at any time.")
 
 # print(student_data.students)
-students = student_data.students
+def lookup_student():
+      students = student_data.students
+      search_name = input("Enter student's full name: ").strip().lower()
+
 
 # You are hired to build a Student Lookup Tool for a school front office. The secretary must be able to:
 
     # Enter a student’s full name
-student_name= input("Enter student full name: ")
-for students if student_name [student_data]:
 
-    if student_name == student ['LName', 'Fname', 'MName']:
-    print(students['CPSID'])
+for students in student:
+        if students ["Combo,Name"].lower() == search_name:
+                print(CPSID)
+                print(HR)
+                print(primary_email)
+                print(FName, LName, MName)
+                print(GL)
+
+
+
+
+
+while True:
+
+
+    if keyboard.is_pressed('q'): 
+        print("Loop terminated by user.")
+        break
+
+
+
+
+
+
+
 
     # Instantly see:
 
@@ -32,7 +65,8 @@ for students if student_name [student_data]:
 
 ## be able to add new data
 # Your program must allow the secretary to ADD a brand new student
-student_name= []
+student_name = []
+item= input
 student_name.append(item)
 
 # into the system while the program is running.
@@ -54,6 +88,7 @@ student_name.append(item)
 
 # 2. Combine the First and Last name into this format:
     #    "Last, First"  
+print("LName", "FName")
 
 # 3. Store all of the new information into ONE dictionary
     #    that matches the structure of the existing student data.
@@ -70,6 +105,3 @@ student_name.append(item)
 # 7. If the CPS ID already exists in the system:
         #    - Do NOT add the student
         #    - Display an error message saying the CPS ID is already taken
-
-
-
